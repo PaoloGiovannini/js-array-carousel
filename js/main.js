f673753ad721f8bcab296b5eaa9561ba1d4acf4b
Paolo Giovannini <giovannini13@gmail.com>
2023-02-28 15:26:44 +0100

    esercizio concluso

--- js/main.js
+++ js/main.js
@@ -8,18 +8,54 @@ for (let i = 0; i < arrayImmagini.length; i++) {
     const contenutoContenitoreImmagine = `<div class="contenitoreImmagine">
                                 <img class="foto" src="${arrayImmagini[i]}">
                             </div>`;
-
-    contenutoDiapositive += contenutoContenitoreImmagine;
-
+    contenutoDiapositive += contenutoContenitoreImmagine; //aggiungo contenuto appena creato
 }
 
-listaImmaginiDom.innerHTML = contenutoDiapositive;
+listaImmaginiDom.innerHTML = contenutoDiapositive; //lo mostro nell'html
 
 const contenitoreImmagineDom = document.getElementsByClassName('contenitoreImmagine'); //array di classi
 
-let immagineCorrente = 0;
+let immagineCorrente = 0; //decido la mia immagine corrente
 
 contenitoreImmagineDom[immagineCorrente].classList.add('block'); //nell'immagine dove mi trovo gli aggiungo la classe block per renderla visibile
+
+const successivoDom = document.getElementById('successivo'); 
+
+const precedenteDom = document.getElementById('precedente');
+
+successivoDom.addEventListener('click', 
+    function() {
+        if (immagineCorrente < contenitoreImmagineDom.length - 1) {
+
+            contenitoreImmagineDom[immagineCorrente].classList.remove('block');
+            immagineCorrente++;
+            contenitoreImmagineDom[immagineCorrente].classList.add('block');
+            precedenteDom.classList.remove('none');
+
+            if (immagineCorrente == contenitoreImmagineDom.length - 1) {
+                successivoDom.classList.add('none');
+            }
+
+        }
+    }
+);
+
+precedenteDom.addEventListener('click', 
+    function() {
+        if (immagineCorrente > 0) {
+
+            contenitoreImmagineDom[immagineCorrente].classList.remove('block');
+            immagineCorrente--;
+            contenitoreImmagineDom[immagineCorrente].classList.add('block');
+            successivoDom.classList.remove('none');
+
+            if (immagineCorrente == 0) {
+                precedenteDom.classList.add('none');
+            }
+
+        }
+    }
+);
 
 
 
