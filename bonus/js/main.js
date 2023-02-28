94e7946aa375aadcac194ce7c18027ae5ad8076c
Paolo Giovannini <giovannini13@gmail.com>
2023-02-28 16:09:34 +0100

    bonus 1 completato

---
+++ bonus/js/main.js
@@ -0,0 +1,60 @@
+const arrayImmagini = ['img/1.webp', 'img/2.webp', 'img/3.webp', 'img/4.webp', 'img/5.webp'];
+
+const listaImmaginiDom = document.querySelector('.listaImmagini');
+
+let contenutoDiapositive = "";
+
+for (let i = 0; i < arrayImmagini.length; i++) {
+    const contenutoContenitoreImmagine = `<div class="contenitoreImmagine">
+                                <img class="foto" src="${arrayImmagini[i]}">
+                            </div>`;
+    contenutoDiapositive += contenutoContenitoreImmagine; //aggiungo contenuto appena creato
+}
+
+listaImmaginiDom.innerHTML = contenutoDiapositive; //lo mostro nell'html
+
+const contenitoreImmagineDom = document.getElementsByClassName('contenitoreImmagine'); //array di classi
+
+let immagineCorrente = 0; //decido la mia immagine corrente
+
+contenitoreImmagineDom[immagineCorrente].classList.add('block'); //nell'immagine dove mi trovo gli aggiungo la classe block per renderla visibile
+
+const successivoDom = document.getElementById('successivo'); 
+
+const precedenteDom = document.getElementById('precedente');
+
+successivoDom.addEventListener('click', 
+    function() {
+        if (immagineCorrente < contenitoreImmagineDom.length - 1) {
+
+            contenitoreImmagineDom[immagineCorrente].classList.remove('block');
+            immagineCorrente++;
+            contenitoreImmagineDom[immagineCorrente].classList.add('block');
+
+        }else{
+            contenitoreImmagineDom[immagineCorrente].classList.remove('block');
+            immagineCorrente = 0;
+            contenitoreImmagineDom[immagineCorrente].classList.add('block');
+        }
+    }
+);
+
+precedenteDom.addEventListener('click', 
+    function() {
+        if (immagineCorrente > 0) {
+
+            contenitoreImmagineDom[immagineCorrente].classList.remove('block');
+            immagineCorrente--;
+            contenitoreImmagineDom[immagineCorrente].classList.add('block');
+            successivoDom.classList.remove('none');
+
+        }else{
+            contenitoreImmagineDom[immagineCorrente].classList.remove('block');
+            immagineCorrente = 4;
+            contenitoreImmagineDom[immagineCorrente].classList.add('block');
+        }
+    }
+);
+
+
+
